90657839eeb362f6e76a4eca7b9ba02641f43a1a
Philemon <orjiphil@gmail.com>
2026-06-28 02:54:09 +0100

    Stopped reading database schema.

--- src/libs/dbConnect.ts
+++ src/libs/dbConnect.ts
@@ -1,6 +1,4 @@
 import mysql from "mysql2/promise";
-import fs from "fs";
-import path from "path";
 import "dotenv/config";
 
 export const connectDatabase = async (databaseName: string) => {
@@ -18,8 +16,7 @@ export const connectDatabase = async (databaseName: string) => {
     let serverConnection: mysql.Connection | null = null;
     try {
         serverConnection = await mysql.createConnection(serverConfig);
-        const mysql_statement = fs.readFileSync(path.join(__dirname, databaseName + "DBSchema.sql"), "utf8");
-        await serverConnection.query(mysql_statement);
+        await serverConnection.query(`Create database if not exists ${databaseName}`);
         console.log(`${databaseName} database has been created successfully.`);
     } catch (error) {
         console.error(`Error connecting to mysql server or creating database ${databaseName}.`);
@@ -43,6 +40,6 @@ export const connectDatabase = async (databaseName: string) => {
     return pool;
  }
 
-export const userDB = connectDatabase("user");
-export const  practiceHistory = connectDatabase("practiceHistory");
-export const jambDB = connectDatabase("jamb");
+export const userDB = connectDatabase("UserDB");
+export const  practiceHistory = connectDatabase("PracticeHistoryDB");
+export const jambDB = connectDatabase("JambDB");
